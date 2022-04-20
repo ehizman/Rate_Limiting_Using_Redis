9dfb4b8e74d68aba9558ab9d367088f95f2d5509
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-04-20 09:49:58 +0100

    made changes

--- src/main/java/com/ehizman/mmr_application/RedisConfig.java
+++ src/main/java/com/ehizman/mmr_application/RedisConfig.java
@@ -3,23 +3,16 @@ package com.ehizman.mmr_application;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
-import io.pivotal.cfenv.core.CfEnv;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.net.URI;
 
 @Configuration
 @Slf4j
@@ -37,7 +30,6 @@ public class RedisConfig {
         return new LettuceConnectionFactory();
     }
 
-    @Bean
     public static StatefulRedisConnection<String, String> connect(){
         RedisURI redisURI = RedisURI.create(System.getenv("REDIS_ENDPOINT_URI"));
         log.info("RedisURI --> {}", redisURI);
