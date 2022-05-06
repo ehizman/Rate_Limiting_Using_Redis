7cbc13f19b0e92491513c4ec0e20ec504357647b
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-05-07 00:10:56 +0100

    changed redis config

--- src/main/java/com/ehizman/mmr_application/MmrApplication.java
+++ src/main/java/com/ehizman/mmr_application/MmrApplication.java
@@ -1,16 +1,10 @@
 package com.ehizman.mmr_application;
 
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.sync.RedisCommands;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import javax.annotation.PostConstruct;
 
 @EnableCaching
 @SpringBootApplication
--- src/main/java/com/ehizman/mmr_application/RedisConfig.java
+++ src/main/java/com/ehizman/mmr_application/RedisConfig.java
@@ -1,22 +1,15 @@
 package com.ehizman.mmr_application;
 
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
-import io.lettuce.core.api.StatefulRedisConnection;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.Protocol;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -25,19 +18,24 @@ import java.net.URISyntaxException;
 @Configuration
 @Slf4j
 public class RedisConfig {
+    @Autowired
+    private ConfigurableEnvironment env;
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        try {
-            URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
-            RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration(
-                    redisURI.getHost(), redisURI.getPort()
-            );
-            return new JedisConnectionFactory(standaloneConfiguration);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var:"+
-                    System.getenv("REDISTOGO_URL"));
+        String[] profiles = env.getActiveProfiles();
+        if (profiles[0].equals("prod")){
+            try {
+                URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+                RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration(
+                        redisURI.getHost(), redisURI.getPort()
+                );
+                return new JedisConnectionFactory(standaloneConfiguration);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var:"+
+                        System.getenv("REDISTOGO_URL"));
+            }
         }
-
+        return new JedisConnectionFactory();
     }
 
     //Creating RedisTemplate for Entity 'PhoneNumber'
