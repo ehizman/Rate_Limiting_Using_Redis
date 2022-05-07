172344ad9cf66345ee7e99d16588266d0f980995
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-05-07 14:21:47 +0100

    changed redis config

--- src/main/java/com/ehizman/mmr_application/RedisConfig.java
+++ src/main/java/com/ehizman/mmr_application/RedisConfig.java
@@ -10,6 +10,9 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
 import java.net.URI;
 import java.net.URISyntaxException;
